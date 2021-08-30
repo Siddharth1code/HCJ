8142aa54dfc750932ea1b8f3e39b241d320acf2e
Siddharth1code <88075593+Siddharth1code@users.noreply.github.com>
2021-08-30 10:44:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var som=[];
+function show;{
+var o1=document.getElementById("so").value;
+var o2=document.getElementById("so1").value;
+var o3=document.getElementById("so2").value;
+som.push(o1);
+som.push(o2);
+som.push(o3);
+document.getElementById("a").innerHTML=st;
+document.getElementById("so1").style.display="none";
+document.getElementById("so2").style.display="inline-block";
+}
+function sort;
+{
+    document.getElementById("d").innerHTML=st;
+}
